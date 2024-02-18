84534b233b90985f96f22284e631e33347a9abdf
Jubair <jubair.ahmed2838@gmail.com>
2024-02-18 16:10:06 +0600

    Add: add task form created with react hook form

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -1,9 +1,77 @@
+import { useForm } from "react-hook-form";
 import Modal from "../ui/Modal";
 
 const AddTaskModal = ({isOpen, setIsOpen}) => {
+
+    // initialize react hook form
+    const {register, handleSubmit, reset, } = useForm()
+
+    const handleModalClose = ()=>{
+        reset()
+        setIsOpen(false)
+    }
+
+    const onSubmit=(data)=>{
+        console.log(data)
+       handleModalClose()
+        
+    }
+
     return(
        <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Add Tasks">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit cum tempora porro, magnam nobis exercitationem eum! Mollitia maiores ratione similique ducimus minima, veritatis libero saepe impedit. Magni quas quisquam deserunt.</p>
+           <form onSubmit={handleSubmit(onSubmit)} className="mt-4 flex flex-col gap-4">
+
+            {/* title */}
+            <div className="flex flex-col gap-2">
+            <label htmlFor="title">Title</label>
+            <input className="w-full rounded-md" type="text" name="title" id="" {...register("title")} />
+            </div>
+
+            {/* description */}
+            <div className="flex flex-col gap-2">
+            <label htmlFor="description">Description</label>
+            <textarea className="w-full rounded-md" name="description" id="" {...register("description")} />
+            </div>
+
+            {/* date */}
+            <div className="flex flex-col gap-2">
+            <label htmlFor="deadline">Deadline</label>
+            <input type="date" className="w-full rounded-md" name="deadline" id="" {...register("deadline")} />
+            </div>
+
+
+            {/* assign to */}
+            <div className="flex flex-col gap-2">
+            <label htmlFor="assign to">Assign to</label>
+            <select defaultValue="default" className="w-full rounded-md" name="assign to" {...register("assign to")}>
+                <option value="default" disabled>Select a member</option>
+                <option value="Joey Tribbiani">Joey Tribbiani</option>
+                <option value="Ross Geller">Ross Geller</option>
+                <option value="Rachel Green">Rachel Green</option>
+            </select> 
+            </div>
+
+             {/* priority */}
+             <div className="flex flex-col gap-2">
+            <label htmlFor="Priority">Priority</label>
+            <select defaultValue="default" className="w-full rounded-md" name="priority" {...register("priority")}>
+                <option value="default" disabled>Choose a priority</option>
+                <option value="High">High</option>
+                <option value="Moderate">Moderate</option>
+                <option value="Low">Low</option>
+            </select> 
+            </div>
+
+           {/* cancel and submit button */}
+           <div className="flex items-center justify-end gap-6 mt-4">
+
+            {/* cancel button */}
+           <button onClick={handleModalClose} className="border-2 border-red-600 px-3 py-2 rounded-md bg-white font-medium text-red-600 duration-200 hover:bg-red-500 hover:text-white hover:cursor-pointer" type="button">Cancel</button>
+
+           {/* add task button */}
+           <button className="border-2 border-blue-600 px-3 py-2 rounded-md bg-white font-medium text-blue-600 duration-200 hover:bg-blue-500 hover:text-white hover:cursor-pointer" type="submit">Add</button>
+           </div>
+           </form>
        </Modal>
     )}
 export default AddTaskModal;
--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -41,7 +41,7 @@ const Modal =({isOpen, setIsOpen, title, children})=> {
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
+                    className="text-lg font-medium leading-6 text-center text-gray-900"
                   >
                    {title}
                   </Dialog.Title>
